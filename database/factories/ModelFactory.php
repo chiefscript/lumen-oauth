974a5a38a93259966016fd5bc1a42ae64d269078
ChiefScript <chiefscript@gmail.com>
2018-05-24 16:59:21 +0300

    Added posts and comments factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,20 +1,24 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
-
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->email,
         'password' => app('hash')->make('pass'),
+        'created_at' => date('Y-m-d H:i:s'),
+        'updated_at' => date('Y-m-d H:i:s'),
+    ];
+});
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentence(4),
+        'content' => $faker->paragraph(4),
+    ];
+});
+
+$factory->define(App\Comment::class, function (Faker\Generator $faker) {
+    return [
+        'content' => $faker->paragraph(1)
     ];
 });
